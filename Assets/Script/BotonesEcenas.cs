81de1cc78c229889d47ed85c8735d1eff1d7e8d5
Saykan247 <127871699+Saykan247@users.noreply.github.com>
2023-05-10 07:16:59 -0700

    menupr

---
+++ Assets/Script/BotonesEcenas.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BotonesEcenas : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+    }
+
+
+    public void CargarNivel(string nombreNivel)
+    {
+        SceneManager.LoadScene(nombreNivel);
+    }
+    public void Salir()
+    {
+        Application.Quit();
+    }
+}
