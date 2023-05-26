94a687fa5b98f631323c5dd12f4190247d748a86
Saykan247 <127871699+Saykan247@users.noreply.github.com>
2023-05-26 09:05:19 -0700

    life

---
+++ Assets/Script/trap.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trap : MonoBehaviour
+{
+    public float speed = 0.01f;
+    public int damage = 10;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.SendMessage("AddDamage", damage);
+        }
+        
+    }
+
+}
