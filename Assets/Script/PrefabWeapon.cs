bb3ea3b97fdfee9e27ab7c1c50f57b4bb2862f10
Saykan247 <127871699+Saykan247@users.noreply.github.com>
2023-07-26 00:58:26 -0700

    armadistancia

---
+++ Assets/Script/PrefabWeapon.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabWeapon : MonoBehaviour {
+
+	public Transform firePoint;
+	public GameObject bulletPrefab;
+    public Animator animator;
+	public Animator animator2;
+
+	void Update () {
+		if (Input.GetButtonDown("Fire2"))
+		{
+			Shoot();
+		}
+	}
+
+	void Shoot ()
+	{
+		Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        
+       animator.SetTrigger("Shoot");
+        
+    }
+    
+
+
+}
