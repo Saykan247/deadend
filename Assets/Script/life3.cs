3d6dedcecc24420f547f5a96a64758b675e09cc2
Saykan247 <127871699+Saykan247@users.noreply.github.com>
2023-07-28 08:08:48 -0700

    nivel 2 mejoras

---
+++ Assets/Script/life3.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class life3 : MonoBehaviour
+{
+    public int totalHealth = 3;
+    public float time = 0.1f;
+    public Image Barrita1;
+
+
+    private float health;
+    private Animator anim;
+    private SpriteRenderer sr;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+
+    void Start()
+    {
+        health = totalHealth;
+
+        Barrita1.fillAmount = health / totalHealth;
+    }
+
+    public void AddDamage(int ammont)
+    {
+        health -= ammont;
+        Debug.Log(health);
+
+        Barrita1.fillAmount = health / totalHealth;
+        anim.SetTrigger("Hit");
+
+        if (health <= 0)
+        {
+            health = 0;
+            SceneManager.LoadScene("Nivel3");
+        }
+
+    }
+
+    public void AddHealth(int ammont)
+    {
+        health += ammont;
+        Debug.Log(health);
+
+        Barrita1.fillAmount = health / totalHealth;
+
+        StartCoroutine(ChangeColor());
+        if (health > totalHealth)
+        {
+            health = totalHealth;
+        }
+    }
+
+    IEnumerator ChangeColor()
+    {
+        sr.color = Color.green;
+        yield return new WaitForSeconds(time);
+        sr.color = Color.white;
+
+    }
+}
